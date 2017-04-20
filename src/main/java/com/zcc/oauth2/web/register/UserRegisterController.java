e01e3ba4b84d568ce4e11d97fc8cde96eac25fdb
bupt_zcc <1215271893@qq.com>
2017-04-20 15:57:01 +0800

    add some new features #1

--- src/main/java/com/zcc/oauth2/web/register/UserRegisterController.java
+++ src/main/java/com/zcc/oauth2/web/register/UserRegisterController.java
@@ -1,9 +1,22 @@
 package com.zcc.oauth2.web.register;
 
+import com.zcc.oauth2.qq.QQConnectException;
+import com.zcc.oauth2.qq.javabeans.AccessToken;
+import com.zcc.oauth2.qq.oauth.Oauth;
 import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class UserRegisterController {
-	
 
+    public String  qqAuthrization(HttpServletRequest request) throws QQConnectException {
+
+
+        AccessToken accessTokenObj=(new Oauth()).getAccessTokenByRequest(request);
+        String accessToken=null;
+        String openID=null;
+        Long expiredIn=0L;
+        return  null;
+    }
 }
